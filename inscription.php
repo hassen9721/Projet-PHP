1936c0aee5d470a866323de27fbb7b39a579d11a
houssemkoubaa <houssem.koubaa2000@gmail.com>
2024-04-12 23:33:14 +0100

    asd

---
+++ inscription.php
@@ -0,0 +1,16 @@
+<?php
+require_once 'pdo.php'; // On inclut la connexion à la bdd
+
+// Retrieve form data
+$nom = $_POST['nom'];
+$prenom = $_POST['prenom'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$isAdmin = 0;
+$connexion = 1;
+
+
+
+// Prepare SQL statement
+$sql = "INSERT INTO users (nom, prenom, email, password, isAdmin,connexion) VALUES ('$nom', '$prenom', '$email', '$password', '$isAdmin', '$connexion')";
+$pdo->exec($sql);
