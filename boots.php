a4ae9e21fb5871ebb41dbd47743d2ef48c5b830f
houssemkoubaa <houssem.koubaa2000@gmail.com>
2024-04-15 01:33:35 +0100

    comande

--- boots.php
+++ boots.php
@@ -18,21 +18,27 @@
 </head>
 
 <body>
+    <?php
+  require_once('product.class.php');
+  $us = new Product();
+  $res = $us->listproducts();
+  ?>
     <br><br><br><br><br><br>
+    <?php foreach ($res as $row) { ?>
+    <?php if ($row['type'] =="chaussure") {?>
 
     <div class="container">
 
         <div class="imgBx">
-            <img src="https://github.com/anuzbvbmaniac/Responsive-Product-Card---CSS-ONLY/blob/master/assets/img/jordan_proto.png?raw=true"
-                alt="Nike Jordan Proto-Lyte Image">
+            <img src="images/<?php echo $row['photo'] ?>" alt="Nike Jordan Proto-Lyte Image">
         </div>
         <div class="details">
             <div class="content">
-                <h2>Jordan Proto-Lyte <br>
-                    <span>Running Collection</span>
-                </h2>
 
-                <p class="product-colors">Available Colors:
+                <h2><?php echo $row['nom'] ?> <br>
+                    <span><?php echo $row['type'] ?></span>
+                </h2>
+                <p class="product-colors">Available Colors:<br>
                     <span class="black active" data-color-primary="#000" data-color-sec="#212121"
                         data-pic="https://github.com/anuzbvbmaniac/Responsive-Product-Card---CSS-ONLY/blob/master/assets/img/jordan_proto.png?raw=true"></span>
                     <span class="red" data-color-primary="#7E021C" data-color-sec="#bd072d"
@@ -40,19 +46,51 @@
                     <span class="orange" data-color-primary="#CE5B39" data-color-sec="#F18557"
                         data-pic="https://github.com/anuzbvbmaniac/Responsive-Product-Card---CSS-ONLY/blob/master/assets/img/jordan_proto_orange_black.png?raw=true"></span>
                 </p>
-                <h3>TND. 12,800</h3><br><br><br><br>
-                <label for="qte">Quantite</label>
-                <input type="number" name="qte" id="qte" onchange="updateTotal()" min="0" />
-                <br><br><br><br>
-                <label for="qte">Totale</label>
 
-                <strong id="total">0</strong>
-                <br><br><br><br>
-                <button>Buy Now</button>
+                <h3>Prix: <?php echo $row['prix'] ?>$</h3>
+
+
+                <div>
+                    <?php if($row['qte'] > 0){ ?>
+                    <b>
+                        <input type="number" id="qte" />
+                    </b>
+                    <?php } else{ ?>
+                    <b>
+                        <br><br>
+                    </b>
+                    <?php } ?>
+                </div>
+                <button onclick="commande()">Buy Now</button>
+                <script>
+                function commande() {
+                    var qte = document.getElementById("qte").value; // Utilisez "document" au lieu de "Document"
+                    if (qte > 0) {
+                        alert("Vous avez commandé " + qte +
+                            " pièces");
+                        document.getElementById("qte").value = 0;
+                    }
+
+                }
+                </script>
+
+                <div style="margin-top:200px;margin-left:50px">
+                    <?php if($row['qte'] > 0){ ?>
+                    <b>
+                        <p style="color:green;">Disponible</p>
+                    </b>
+                    <?php } else{ ?>
+                    <b>
+                        <p style="color:red;">Non disponible</p>
+                    </b>
+                    <?php } ?>
+                </div>
+
             </div>
         </div>
     </div>
-
+    <?php } ?>
+    <?php } ?>
     <script>
     // Change The Picture and Associated Element Color when Color Options Are Clicked.
     $(".product-colors span").click(function() {
@@ -66,14 +104,6 @@
         $(".container .details button").css("background", $(this).attr("data-color-sec"));
         $(".imgBx img").attr('src', $(this).attr("data-pic"));
     });
-
-    function updateTotal() {
-        var prix = 12.800; // or you can fetch this value dynamically if it changes
-        var qteInput = document.getElementById("qte");
-        var qte = qteInput.value;
-        var total = prix * qte;
-        document.getElementById("total").textContent = "TND. " + total.toFixed(2);
-    }
     </script>
 </body>
 
--- watchs.php
+++ watchs.php
@@ -18,44 +18,92 @@
 </head>
 
 <body>
+    <?php
+  require_once('product.class.php');
+  $us = new Product();
+  $res = $us->listproducts();
+  ?>
     <br><br><br><br><br><br>
+    <?php foreach ($res as $row) { ?>
+    <?php if ($row['type'] =="montre") {?>
 
     <div class="container">
 
         <div class="imgBx">
-            <img src="https://github.com/anuzbvbmaniac/Responsive-Product-Card---CSS-ONLY/blob/master/assets/img/jordan_proto.png?raw=true"
-                alt="Nike Jordan Proto-Lyte Image">
+            <img src="images/<?php echo $row['photo'] ?>" alt="Nike Jordan Proto-Lyte Image">
         </div>
         <div class="details">
             <div class="content">
-                <h2>mongela <br>
-                    <span></span>
-                </h2>
-                <br>
 
-                <h3>TND. 12,800</h3><br>
-                <br><br><br><br>
-                <label for="qte">Quantite</label>
-                <input type="number" name="qte" id="qte" onchange="updateTotal()" min="0" />
-                <br><br><br><br>
-                <label for="qte">Totale</label>
-                <strong id="total">0</strong>
-                <br><br><br><br>
-                <button>Buy Now</button>
+                <h2><?php echo $row['nom'] ?> <br>
+                    <span><?php echo $row['type'] ?></span>
+                </h2>
+                <p class="product-colors">Available Colors:<br>
+                    <span class="black active" data-color-primary="#000" data-color-sec="#212121"
+                        data-pic="https://github.com/anuzbvbmaniac/Responsive-Product-Card---CSS-ONLY/blob/master/assets/img/jordan_proto.png?raw=true"></span>
+                    <span class="red" data-color-primary="#7E021C" data-color-sec="#bd072d"
+                        data-pic="https://github.com/anuzbvbmaniac/Responsive-Product-Card---CSS-ONLY/blob/master/assets/img/jordan_proto_red_black.png?raw=true"></span>
+                    <span class="orange" data-color-primary="#CE5B39" data-color-sec="#F18557"
+                        data-pic="https://github.com/anuzbvbmaniac/Responsive-Product-Card---CSS-ONLY/blob/master/assets/img/jordan_proto_orange_black.png?raw=true"></span>
+                </p>
+
+                <h3>Prix: <?php echo $row['prix'] ?>$</h3>
+
+
+                <div>
+                    <?php if($row['qte'] > 0){ ?>
+                    <b>
+                        <input type="number" id="qte" />
+                    </b>
+                    <?php } else{ ?>
+                    <b>
+                        <br><br>
+                    </b>
+                    <?php } ?>
+                </div>
+                <button onclick="commande()">Buy Now</button>
                 <script>
-                function updateTotal() {
-                    var prix = 12.800; // or you can fetch this value dynamically if it changes
-                    var qteInput = document.getElementById("qte");
-                    var qte = qteInput.value;
-                    var total = prix * qte;
-                    document.getElementById("total").textContent = "TND. " + total.toFixed(2);
+                function commande() {
+                    var qte = document.getElementById("qte").value; // Utilisez "document" au lieu de "Document"
+                    if (qte > 0) {
+                        alert("Vous avez commandé " + qte +
+                            " pièces");
+                        document.getElementById("qte").value = 0;
+                    }
+
                 }
                 </script>
+
+                <div style="margin-top:200px;margin-left:50px">
+                    <?php if($row['qte'] > 0){ ?>
+                    <b>
+                        <p style="color:green;">Disponible</p>
+                    </b>
+                    <?php } else{ ?>
+                    <b>
+                        <p style="color:red;">Non disponible</p>
+                    </b>
+                    <?php } ?>
+                </div>
+
             </div>
         </div>
     </div>
-
+    <?php } ?>
+    <?php } ?>
     <script>
+    // Change The Picture and Associated Element Color when Color Options Are Clicked.
+    $(".product-colors span").click(function() {
+        $(".product-colors span").removeClass("active");
+        $(this).addClass("active");
+        $(".active").css("border-color", $(this).attr("data-color-sec"))
+        $("body").css("background", $(this).attr("data-color-primary"));
+        $(".content h2").css("color", $(this).attr("data-color-sec"));
+        $(".content h3").css("color", $(this).attr("data-color-sec"));
+        $(".container .imgBx").css("background", $(this).attr("data-color-sec"));
+        $(".container .details button").css("background", $(this).attr("data-color-sec"));
+        $(".imgBx img").attr('src', $(this).attr("data-pic"));
+    });
     </script>
 </body>
 
